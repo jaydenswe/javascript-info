3e1d3fb50aea08ad6d1d956f7912b1cc63feab1c
jaydenswe <247421157+jaydenswe@users.noreply.github.com>
2026-03-12 20:47:44 +0000

    Completed second exercise and passed tests

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ function camelize(str) {
   const newStr = capitaliseFirstLetters.join('')
   return newStr;
 }
+
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array. The function should not modify the array. It should return the new array.
+function filterRange(arr, a, b) {
+  const filteredArray = arr.filter((num) => {
+      if (num >= a && num <= b) {
+        return num
+      }
+    }) 
+  return filteredArray
+}
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4)
+filterRange(arr, 1, 4);
